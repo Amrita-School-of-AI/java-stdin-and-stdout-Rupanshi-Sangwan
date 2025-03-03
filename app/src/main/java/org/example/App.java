1ea9610127498f852378ff5cfe3bd876f91bc0e6
Rupanshi-Sangwan <rupanshi06sangwan@gmail.com>
2025-03-03 10:28:09 +0530

    assignment over

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ public class App {
         int b = scan.nextInt();
         int c = scan.nextInt();
 
+        scan.close();
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
